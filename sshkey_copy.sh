c50bb3b7a2d329fd3633b619cb4ed642f63c08e0
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-23 18:32:55 +0300

    Create sshkey_copy.sh

---
+++ sshkey_copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to copy SSH keys between servers
+
+echo "Enter the IP or hostname of the DEST server:"
+read dest_server
+echo "Enter the username for the DEST server:"
+read dest_username
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    echo "No ssh key pair found on the source server. Generating one with ssh-keygen"
+    ssh-keygen -t rsa
+fi
+
+ssh-copy-id $dest_username@$dest_server
+
+if [ $? -eq 0 ]; then
+  echo "Key has been copied to $dest_username@$dest_server"
+else
+  echo "Error copying the key to $dest_username@$dest_server"
+fi
